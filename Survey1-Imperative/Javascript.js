9e3a3913af070d12dd94484b47451e4d5485553c
Timmy <timjimski@gmail.com>
2017-03-16 11:23:09 -0400

    Wrote code for Javascript. Need to add comments.

---
+++ Survey1-Imperative/Javascript.js
@@ -0,0 +1,31 @@
+/*
+CS310 Survey 1 - Imperative languages
+Team: Haskers
+Author: Timothy Scott
+*/
+
+//Main function starts here:
+
+for(var i = 0; i < 10; i++){
+	console.log("Catalan number " + i + " is " + catalan(i));
+}
+
+//Actual functions start here
+function catalan(n){
+
+	return (1 / (n + 1)) * binomialCoeff(2*n, n);
+
+	function binomialCoeff(n, k){
+		// Within this nested function, "n" refers to the parameter of binomialCoeff()
+		// "n" does NOT refer to the parameter of catalan()
+		return factorial(n) / (factorial(k) * factorial(n - k));
+
+		function factorial(n){
+			if(n <= 1){
+				return 1;
+			}else{
+				return n * factorial(n - 1);
+			}
+		}
+	}
+}
