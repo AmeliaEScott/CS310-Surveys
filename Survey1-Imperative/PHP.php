385743795b3c9cef5818625bcba8fe8adff74892
rosier10 <rmrosier@mix.wvu.edu>
2017-03-18 15:01:24 -0400

    Fixed php so its working now.

--- Survey1-Imperative/PHP.php
+++ Survey1-Imperative/PHP.php
@@ -7,34 +7,33 @@
  */
 
 //Below is a for loop to test the catalan function for inputs 1 through 10
-for($t=1; $t<11; $t++){
+for($t=0; $t<11; $t++){
     echo "Catalan number ";
     echo $t;
     echo " is ";
     echo catalanNumber($t);
+    echo "\n";
 }
-
 //Below is the catalan function that returns  1/n+1 times the binomial coefficient
 function catalanNumber($n){
-    echo "test1";
-    return (1/($n+1))*binomialCoefficient(2*$n,$n);
-
     //Below is the binomial coefficient function that return j!/(k!*(j-k)!)
-    function binomialCoefficient($j,$k){
-        echo "test2";
-        return factorial($j)/(factorial($k)*factorial($j-$k));
+    $binomialCoefficient = function($j,$k){
 
         //Below is the factorial function that recursively finds i!
-        function factorial($i){
-            echo "test3";
-            if($i<2){
-                return 1;
+        $factorial = function($i){
+            $fact=1;
+            for($l=$i;$l>0;$l--){
+                if($i<2){
+                    return 1;
+                }
+                else{
+                    $fact = $fact*$l;
+                }
             }
-            else{
-                return $i*factorial($i--);
-            }
-        }//end factorial()
-        echo "test4";
-    }//end binomailCoefficient
+            return $fact;
+        };//end factorial()
+        return $factorial($j)/($factorial($k)*$factorial($j-$k));
+    };//end binomailCoefficient
+    return (1/($n+1))*$binomialCoefficient(2*$n,$n);
 }//end catalanNumber
 ?>
