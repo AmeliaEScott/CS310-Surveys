dcc01c4859dbe40d4c1dc964ec924cd24eff5766
Edwin Velasquez <evelasqu@mix.wvu.edu>
2017-03-23 15:16:45 -0400

    Changed functions to be nested

--- Survey1-Imperative/ruby.rb
+++ Survey1-Imperative/ruby.rb
@@ -1,23 +1,21 @@
-# this method returns the factorial of n
-def fact(n)
-	if n == 0
-		1
-	else
-		n * fact(n-1)
-	end
-end
-
-=begin this method returns the binomial coefficient, 
-commonly known as n choose k or combination, 
-=end of n and k
-def binCo(n, k)
-	fact(n)/(fact(k)*fact(n-k))
-end
-
-# this method prints the catalan number of n
+# Returns the nth catalan number
 def cataNum(n)
-	fact(2*n)/(fact(n+1)*fact(n))
-end
+	# Returns the value of n choose k
+	def binCo(n, k)
+		# Returns the factorial of n
+		def fact(n)
+			if n == 0
+				1
+			else
+				n * fact(n-1)
+			end
+		end # End fact
+		
+		fact(n)/(fact(k)*fact(n-k))
+	end # End binCo
+
+	((1 / (n.to_f + 1)) * binCo(2 * n, n)).to_i
+end # End cataNum
 
 # printing the catalan number of 0-9
 for i in 0..9
