cc8c652fc9635b66eecc02c686b80800a135e407
Timothy Scott <timjimski@gmail.com>
2017-04-12 20:00:56 -0400

    Fixed clojure

--- Survey2-Functional/same.clj
+++ Survey2-Functional/same.clj
@@ -6,9 +6,9 @@
 
 (defn same [list1, list2]
       (cond
-      ((empty? list1) (empty? list2))
-      ((empty? list2) false)
-      ((= (first list1) (first list2))
-      	  (same (rest list1) (rest list2)))
+      (empty? list1) (empty? list2)
+      (empty? list2) false
+      (= (first list1) (first list2))
+      	  (same (rest list1) (rest list2))
 	  :else false)
 	  )
