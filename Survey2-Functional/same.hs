46569fa9905f79841962f3a57394b5c0399d4dfb
Timothy Scott <timjimski@gmail.com>
2017-04-12 21:48:54 -0400

    Fixed Haskell issue

--- Survey2-Functional/same.hs
+++ Survey2-Functional/same.hs
@@ -4,11 +4,11 @@
 -- Team: Haskers
 
 same list1 list2 =
-     if (length list1 == 0) && (length list2 == 0) then true
-     else if length list2 == 0 then false
+     if (length list1 == 0) && (length list2 == 0) then True
+     else if length list2 == 0 then False
      else if (head list1) == (head list2)
      	  then same (tail list1) (tail list2)
-     else false
+     else False
 
-main = same ([1, 2, 3]) ([3, 2, 1])
+main = print (same ([1, 2, 3]) ([1, 2, 3]))
 	
