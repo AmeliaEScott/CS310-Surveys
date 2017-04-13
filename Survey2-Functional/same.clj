d5a848a513df52ea6eea6ddd8429cb2611360694
Timothy Scott <timjimski@gmail.com>
2017-04-12 22:22:12 -0400

    Changed formatting for report

--- Survey2-Functional/same.clj
+++ Survey2-Functional/same.clj
@@ -5,10 +5,10 @@
 
 
 (defn same [list1, list2]
-      (cond
-      (empty? list1) (empty? list2)
-      (empty? list2) false
-      (= (first list1) (first list2))
-      	  (same (rest list1) (rest list2))
-	  :else false)
-	  )
+	(cond
+	(empty? list1) (empty? list2)
+	(empty? list2) false
+	(= (first list1) (first list2))
+		(same (rest list1) (rest list2))
+	:else false)
+)
