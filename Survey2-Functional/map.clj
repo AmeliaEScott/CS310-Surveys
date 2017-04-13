d3cdf99b8aae2a535321def1a8ed0ce7e367860d
Matt Gramlich <mpgramlich@mix.wvu.edu>
2017-04-12 22:25:21 -0400

    finished map function for clojure

---
+++ Survey2-Functional/map.clj
@@ -0,0 +1,13 @@
+
+
+(defn testfunc [a] (* a a) )
+ 
+(defn mapFunc [fun v] 
+  (cons (fun (first v))
+	  (if (not (empty? (subvec v 1))) 
+		(mapFunc fun (subvec v 1))
+	   )
+   )
+)
+
+(print (mapFunc testfunc [3 1 2 5]) )
